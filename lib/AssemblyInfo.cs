e276d38e688c7d99386a5acf30e336e89e3f5830
Allister Beharry <allister.beharry@gmail.com>
2017-10-06 18:15:21 -0400

    Internals visible to AzSync.

--- lib/AssemblyInfo.cs
+++ lib/AssemblyInfo.cs
@@ -4,9 +4,11 @@
 // </copyright>
 //------------------------------------------------------------------------------
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Microsoft.WindowsAzure.Storage.DataMovement.dll")]
 [assembly: AssemblyDescription("")]
+[assembly: InternalsVisibleTo("AzSync")]
